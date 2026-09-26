9e22f517a2b931033eb16d7a0ee6b89517c1549c
Nidhin R <nidhinjunior117@gmail.com>
2026-09-26 20:05:41 +0530

    Add Microsoft clone JavaScript interactions

---
+++ Microsoft-Clone/script.js
@@ -0,0 +1,10 @@
+const learnMoreButton = document.getElementById("learnMore");
+const exploreButton = document.getElementById("exploreButton");
+
+learnMoreButton.addEventListener("click", function () {
+    alert("Welcome to Microsoft 365!");
+});
+
+exploreButton.addEventListener("click", function () {
+    alert("Explore Microsoft products and services.");
+});
